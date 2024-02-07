ce8a161ce81bc27b8077113bdf6d7251bade78e1
Isis <isissouzaguimaraes@gmail.com>
2024-02-07 09:14:45 -0300

    tela inicial completa

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main className="Caol">
+        <h1>Informações do site Caol</h1>
+
+        <div class="botoes">
+          <h2>Aba consultor</h2>
+          <div className="botoes-consultor">
+          <div className="conteudo-botao">
+            <a href="/resultado-do-botao-relatorio">
+              <button>Relatório consultor</button>
+            </a>
+          </div>
+
+          <div className="conteudo-botao">
+            <a href="/resultado-do-botao-grafico">
+              <button>Gráfico consultor</button>
+            </a>
+          </div>
+          </div>
+        </div>
+
+        <div class="botoes">
+          <h1>Aba cliente</h1>
+          <div className="botoes-cliente">
+          <div className="conteudo-botao">
+            <a href="/resultado-do-botao-relatorio-cliente">
+              <button>Relatório cliente</button>
+            </a>
+          </div>
+
+          <div className="conteudo-botao">
+            <a href="/resultado-do-botao-grafico-cliente">
+              <button>Gráfico cliente</button>
+            </a>
+          </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
