63ae96e4cb9453be519d228520682d948553ca75
Isis <isissouzaguimaraes@gmail.com>
2024-02-07 14:30:38 -0300

    relatorios arrumados

--- src/components/RelatorioCliente.js
+++ src/components/RelatorioCliente.js
@@ -1,12 +1,23 @@
 import React from "react";
-import "../styles/stylesComponents.css"
+import resultados from "./relatorioCliente.json";
+import "../styles/stylesComponents.css";
 
 function RelatorioCliente() {
     return (
         <div className="relatorio-cliente">
             <h1> Relatorio Cliente</h1>
+            {resultados.map(resultado => (
+                <div key={resultado.co_cliente} className={`conteudoRelatorio ${resultado.is_champion === 1 ? 'bg-blue-200' : 'bg-transparent'}`}>
+                    <h4>Nome de Contato: <br />{resultado.no_contato}</h4>
+                    <h5>Número de Telefone: <br />{resultado.nu_telefone}</h5>
+                    <h4>Mês de Emissão: <br />{resultado.mes_referencia}</h4>
+                    <h5>Receita: <br />{resultado.receita}</h5>
+                    <div className={`champion-badge ${resultado.is_champion === 1 ? 'champion-true' : 'champion-false'}`}></div>
+                    {resultado.is_champion === 1 ? <h3 className="champion"></h3> : <h3 className="champion-false"></h3>}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 export default RelatorioCliente;
--- src/components/RelatorioConsultor.js
+++ src/components/RelatorioConsultor.js
@@ -1,12 +1,23 @@
 import React from "react";
+import relatorioConsultorData from "./relatorioConsultor.json";
 
 function RelatorioConsultor() {
   return (
-    <>
-      <main className="relatorio-consultor">
-        <h1> Relatorio Consultor</h1>
-      </main>
-    </>
+    <div className="relatorio-consultor">
+      <h1>Relatório Consultor</h1>
+      {relatorioConsultorData.map((resultado, index) => (
+        <div key={index} className="conteudoRelatorio">
+          <p className="usuario">Usuário: <br/>{resultado.no_usuario}</p>
+          <p>Sistema: <br/>{resultado.co_sistema}</p>
+          <p>Ativo: <br/>{resultado.in_ativo}</p>
+          <p>Tipo Usuário: <br/>{resultado.co_tipo_usuario}</p>
+          <p>Receita: <br/>{resultado.receita_liquida}</p>
+          <p>Custo: <br/>{resultado.custo_fixo}</p>
+          <p>Comissão: <br/>{resultado.comissao}</p>
+          <p>Lucro: <br/>{resultado.lucro}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
